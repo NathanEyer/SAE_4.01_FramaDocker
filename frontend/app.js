46c845cc8a7e78a9cab44302448f317fa494114e
Nathan <144025066+NathanEyer@users.noreply.github.com>
2025-06-05 15:53:13 +0200

    Ajout index.ejs

--- frontend/app.js
+++ frontend/app.js
@@ -8,8 +8,18 @@ const PORT = 3000;
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+app.use(express.json());
+app.use(express.static('frontend/public'));
+
 app.get('/', (req, res) => {
     res.render('index');
+});
+
+app.get('/poll/:id', (req, res) => {
+    res.render('index', { 
+        title: 'Sondage - Framadate Clone',
+        subtitle: 'Votez pour vos disponibilités'
+    });
 });
 
 // Start the server
